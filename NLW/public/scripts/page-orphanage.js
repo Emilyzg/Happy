d32f0d1b6f3d3e4fff737f4724c1141d33e139d6
Emily Zea G <emilyzea55@gmail.com>
2020-10-15 20:23:06 -0300

    NLW

--- NLW/public/scripts/page-orphanage.js
+++ NLW/public/scripts/page-orphanage.js
@@ -1,16 +1,16 @@
 const options = {
-    dragging: false,
-    touchZoom: false,
-    doubleClickZoom: false,
-    scrollWheelZoom: false,
-    zoomControl: false,
+  dragging: false,
+  touchZoom: false,
+  doubleClickZoom: false,
+  scrollWheelZoom: false,
+  zoomControl: false,
 }
 
 //create map
-const map = L.map("mapid", options).setView([-27.2197525, -49.6506357], 15);
+const map = L.map("mapid", options).setView([-27.2197525, -49.6506357], 15)
 
 //create and add tileLayter
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map)
 
 //create icon
 const icon = L.icon({
@@ -18,8 +18,28 @@ const icon = L.icon({
   iconSize: [58, 68],
   iconAnchor: [29, 68],
   popupAnchor: [170, 2],
-});
+})
 
 //create and add marker
-L.marker([-27.2197525, -49.6506357], { icon })
-.addTo(map)
+L.marker([-27.2197525, -49.6506357], { icon }).addTo(map);
+
+// imagem gallery
+function selectImage(event) {
+  const button = event.currentTarget
+
+  //remover todas as classes .active
+  const buttons = document.querySelectorAll(".images button")
+  buttons.forEach(removeActiveClass)
+
+  function removeActiveClass(button) {
+  }
+
+  //selecionar a imagem clicada
+  const image = button.children[0]
+  const imageContainer = document.querySelector(".orphanage-details > img")
+
+  //atualizar o conteiner da imagem
+  imageContainer.src = image.src
+
+  button.classList.add("active");
+}
